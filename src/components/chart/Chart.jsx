9573d8c334eaf51154be7d3dd26018467468e399
Nikunj <nikunjadm@gmail.com>
2022-10-15 10:31:16 +0530

    User page complete

--- src/components/chart/Chart.jsx
+++ src/components/chart/Chart.jsx
@@ -10,7 +10,7 @@ import {
   } from "recharts";
   
 
-export default function Chart() {
+export default function Chart({aspect,title}) {
     const data = [
         { name: "January", Total: 1200 },
         { name: "February", Total: 2100 },
@@ -21,8 +21,8 @@ export default function Chart() {
       ];
   return (
     <div className="chart">
-      <div className="title">Last 6 Months (Revenue)</div>
-      <ResponsiveContainer width="100%" aspect={2/1}>
+      <div className="title">{title}</div>
+      <ResponsiveContainer width="100%" aspect={aspect}>
         <AreaChart
           width={730}
           height={250}
